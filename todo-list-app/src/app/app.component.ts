7b576acbecc29715650578177baff18699e61b21
System Administrator <root@MacBook-Air-de-Miguel.local>
2023-06-02 23:10:09 -0600

    add segundo commit

--- todo-list-app/src/app/app.component.ts
+++ todo-list-app/src/app/app.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   title = 'todo-list-app';
-  
+  list: any[] = [];
+  newTask: any={};
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.list.push({ id: 1, name: 'Limpiar habitación 402' });
+
+  }
+  add(): void {
+    if(!this.newTask.name)
+    return;
+    this.list.push(this.newTask);
+    this.newTask={};
   }
 
+  delete(item: any): void {
+    if(!item.name)
+    return;
+    debugger;
+    let index = this.list.findIndex(x => x.name == item.name);
+    //delete this.list[index];
+    this.list.splice(index,1);
+  }
 }
